e6dee55308ade4dbffa54553a153b1d095dce8ed
Md Tanjin Alam <31115996+TanjinAlam@users.noreply.github.com>
2022-08-30 21:35:14 +0600

    Time: 8 ms (70.51%), Space: 7.3 MB (12.03%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -2,15 +2,36 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         map<char,char>m;
+        map<char,char>m1;
         map<char,bool>track;
         for(int i=0; i<s.size(); i++){
             if(!m[s[i]] && !track[t[i]]){
                 m[s[i]] = t[i];
+                m1[t[i]] = s[i];
                 track[t[i]] = true;
-            } else if(m[s[i]] != t[i]){
+            }
+        }
+        map<char, char>::iterator itr;
+        for (itr = m.begin(); itr != m.end(); ++itr) {
+            cout << '\t' << itr->first << '\t' << itr->second
+                 << '\n';
+        }
+        for (itr = m1.begin(); itr != m1.end(); ++itr) {
+            cout << '\t' << itr->first << '\t' << itr->second
+                 << '\n';
+        }
+        for(int i=0; i<t.size(); i++){
+            if(m1[t[i]]){
+                t[i] = m1[t[i]];
+            }else {
                 return false;
             }
         }
-        return true;
+        cout << "ttttt"<< t << endl;
+        if(s==t){
+            return true;
+        } else {
+             return false;
+        }
     }
 };
